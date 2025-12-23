e992583438d0efed689b9f138eb1870361eec519
Ananya <ananyapagolu25@gmail.com>
2025-12-23 22:25:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function saveMood(mood) {
+    let moods = JSON.parse(localStorage.getItem("moods")) || [];
+    moods.push({ mood: mood, date: new Date().toLocaleString() });
+    localStorage.setItem("moods", JSON.stringify(moods));
+    alert("Mood saved successfully!");
+}
+
+function startBreathing() {
+    const text = document.getElementById("breathing-text");
+    let steps = ["Inhale...", "Hold...", "Exhale..."];
+    let index = 0;
+
+    text.innerText = steps[index];
+
+    let interval = setInterval(() => {
+        index++;
+        if (index === steps.length) {
+            clearInterval(interval);
+            text.innerText = "Repeat if needed 🌿";
+        } else {
+            text.innerText = steps[index];
+        }
+    }, 3000);
+}
+
+function saveJournal() {
+    let entry = document.getElementById("journal-input").value;
+
+    if (entry.trim() === "") {
+        alert("Please write something before saving.");
+        return;
+    }
+
+    let journal = JSON.parse(localStorage.getItem("journal")) || [];
+    journal.push({ entry: entry, date: new Date().toLocaleString() });
+    localStorage.setItem("journal", JSON.stringify(journal));
+
+    document.getElementById("journal-input").value = "";
+    alert("Journal entry saved!");
+}
